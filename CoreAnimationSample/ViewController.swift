73e5684e5d6532df5b4b8b01a227017d8ca10971
Gonzalo Campos <gonzaloc@spotify.com>
2023-06-29 23:31:11 -0600

    Add view to app.

--- CoreAnimationSample/ViewController.swift
+++ CoreAnimationSample/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 100))
+        myView = UIView(frame: frame)
+        myView.backgroundColor = .blue
+        view.addSubview(myView)
     }
 
-
+//    func experiment() {
+//        let myView = UIView()
+//        let layer = myView.layer
+//    }
 }
 
